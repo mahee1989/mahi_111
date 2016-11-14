b85f1f557f76da476272c512625b6b2736516b93
mahesh <mahesh@ubuntu.(none)>
2016-11-13 23:38:29 -0800

    modified

--- 17-10-2016/mmm.sh
+++ 17-10-2016/mmm.sh
@@ -1,10 +1,10 @@
-As with many great things in life, Git began with a bit of creative destruction and fiery controversy.
+s with many great things in life, Git began with a bit of creative destruction and fiery controversy.
 
 The Linux kernel is an open source software project of fairly large scope. For most of the lifetime of the Linux kernel maintenance (1991–2002), changes to the software were passed around as patches and archived files. In 2002, the Linux kernel project began using a proprietary DVCS called BitKeeper.
 
 In 2005, the relationship between the community that developed the Linux kernel and the commercial company that developed BitKeeper broke down, and the tool’s free-of-charge status was revoked. This prompted the Linux development community (and in particular Linus Torvalds, the creator of Linux) to develop their own tool based on some of the lessons they learned while using BitKeeper. Some of the goals of the new system were as follows:
 
-Speed
+
 
 Simple design
 
